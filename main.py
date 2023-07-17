b61b16c755376e89178a775c5cccd9d631834200
UeFrog <77925186+UeFrog@users.noreply.github.com>
2023-07-17 15:59:39 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import streamlit as st
 import keras_ocr
 pipeline = keras_ocr.pipeline.Pipeline()
 p = "img.jpg"
 images = [keras_ocr.tools.read(p)]
 prediction = pipeline.recognize(images)
+st.write("Success")
 print("hello")
